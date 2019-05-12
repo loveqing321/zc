506ef5e8172d4fd5f71c94cbd5da453c26b2234e
loveqing3211 <andycooler@163.com>
2019-05-12 14:16:35 +0800

    初始化后端模块

--- zc-parent/zc-merchant-console/src/main/java/top/zuche/merchant/console/system/service/UserDetailsServiceImpl.java
+++ zc-parent/zc-merchant-console/src/main/java/top/zuche/merchant/console/system/service/UserDetailsServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import top.zuche.merchant.console.system.dto.UserDetailsImpl;
 import top.zuche.services.api.dto.UserDTO;
+import top.zuche.services.api.exception.ServiceException;
 import top.zuche.services.api.service.UserRpcService;
 
 /**
@@ -22,23 +23,26 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserDTO userDTO = userRpcService.queryUserByUsername(username);
-        if (userDTO == null) {
-            return null;
+        try {
+            UserDTO userDTO = userRpcService.queryUserByUsername(username);
+            if (userDTO == null) {
+                throw new UsernameNotFoundException(null);
+            }
+            UserDetailsImpl userDetails = new UserDetailsImpl();
+            userDetails.setUsername(userDTO.getUsername());
+            userDetails.setPassword(userDTO.getPassword());
+            userDetails.setAccountNonExpired(!boolValue(userDTO.getAccountExpired()));
+            userDetails.setAccountNonLocked(!boolValue(userDTO.getAccountLocked()));
+            userDetails.setCredentialsNonExpired(!boolValue(userDTO.getCredentialsExpired()));
+            userDetails.setUserId(userDTO.getId());
+            userDetails.setMerchantId(userDTO.getMerchantId());
+            userDetails.setEnabled(!boolValue(userDTO.getIsDeleted()));
+            userDetails.setTelephone(userDTO.getTelephone());
+            userDetails.setPost(userDTO.getPost());
+            return userDetails;
+        } catch (ServiceException e) {
+            throw new UsernameNotFoundException(null);
         }
-        UserDetailsImpl userDetails = new UserDetailsImpl();
-        userDetails.setUsername(userDTO.getUsername());
-        userDetails.setPassword(userDTO.getPassword());
-        userDetails.setAccountNonExpired(!boolValue(userDTO.getAccountExpired()));
-        userDetails.setAccountNonLocked(!boolValue(userDTO.getAccountLocked()));
-        userDetails.setCredentialsNonExpired(!boolValue(userDTO.getCredentialsExpired()));
-        userDetails.setUserId(userDTO.getId());
-        userDetails.setMerchantId(userDTO.getMerchantId());
-        userDetails.setEnabled(!boolValue(userDTO.getIsDeleted()));
-        userDetails.setTelephone(userDTO.getTelephone());
-        userDetails.setPost(userDTO.getPost());
-
-        return userDetails;
     }
 
     /**
